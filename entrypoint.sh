5004ccec3dbbb7c287435c1acaf0bb9566ca6ba4
Martial Michel <mmartial@users.noreply.github.com>
2022-04-10 12:47:46 -0400

    Entrypoint only generte the configuration file the first time

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,23 +20,39 @@ fi
 cfile="/etc/hyprspace/hs.yaml"
 rfile="/app/config.yml"
 if [ ! -f ${cfile} ]; then
+    echo "[*****] No configuration file (${cifle}) found, generating one"
     ${tool} init hs
     if [ ! -f ${cfile} ]; then
       echo "Failed to initialize"
       exit 1
     fi
     if [ -f ${rfile} ]; then
+      echo "[*****] Specialization file (${rfile}) found, applying"
       cp ${cfile} /tmp/hs.yaml
       perl /app/repl.pl ${rfile} ${cfile} | tee /tmp/hs.yaml.mod
       cp /tmp/hs.yaml.mod ${cfile}
     fi
 fi
 
-# Start it
-#$tool up hs0 &
+if [ ! -f ${cfile} ]; then
+  echo "[*****] No configuration file (${cfile}) found, aborting"
+  exit 1
+fi
+echo "[*****] Configuration file: ${cfile}"
+cat ${cfile}
+echo "[*****] End configuration file"
+echo ""
 
-# Configure iptables for 10.11.12.x
-# [...]
+# Start it
+$tool up hs &
+
+##  Configure iptables for 10.11.12.x
+# Allow previously established connection
+# iptables -A INPUT -s 10.11.12.0/24 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# Allow .11 to access ssh
+# iptables -A INPUT -s 10.11.12.11/32 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT 
+# Drop any other request from anyone on the HP range
+# iptables -I INPUT -m iprange --src-range 10.11.12.2-10.11.12.255 -j DROP
 
 # Wait until manual Stop
 sleep infinity
